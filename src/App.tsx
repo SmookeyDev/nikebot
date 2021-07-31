2a0e8377bccf38aebf9c29d9800ada2c632807e2
SmookeyDev <smookeydev@gmail.com>
2021-07-31 15:50:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Bottombar from './components/bottombar';
+
+function App() {
+  return (
+    <div>
+      <div className="min-h-screen bg-gray-200"></div>
+      <Bottombar/>
+    </div>
+    
+  );
+}
+
+export default App;
